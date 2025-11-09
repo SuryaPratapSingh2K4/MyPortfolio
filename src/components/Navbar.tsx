9e4b2c97cbc0854dec9f1356bc6cb6d9578aa5a2
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-11-09 14:42:50 +0530

    naavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Navbar:React.FC = () => (
-    <header className='top-0 bg-black border-b border-purple-800'>
+    <header className='top-0 bg-black border-b border-purple-800 flex-1'>
         <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
             <div className='flex flex-row gap-3 items-center'>
                 <h1 className='text-lg md:text-xl lg:text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-indigo-400 '>Surya Pratap Singh</h1>
