c34909c4fec650267151405db4d6bb6e49cf115c
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-11-09 15:25:37 +0530

    n7

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
 
-const Navbar:React.FC = () => (
-    <header className='top-0 w-full left-0 bg-black border-b border-purple-800 px-10'>
-        <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
-            <div className='flex flex-row gap-3 items-center'>
-                <h1 className='text-lg md:text-xl lg:text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-indigo-400 '>Surya Pratap Singh</h1>
-                <p className='text-xs  text-gray-400'>Frontend & MERN Developer</p>
-            </div>
-            <div className='hidden md:flex gap-8 '>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#about">About</a>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#experience">Experience</a>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#skills">Tech</a>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#projects">Projects</a>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#education">Education</a>
-                <a className='text-gray-400 text-xs hover:text-purple-300 hover:font-bold' href="#contact">Contact</a>
-            </div>
-        </div>
-    </header>
+const Navbar: React.FC = () => (
+  <header className="fixed top-0 left-0 w-full bg-black border-b border-purple-800">
+    <div className="max-w-6xl mx-auto px-6 sm:px-10 md:px-16 py-4 flex items-center justify-between">
+      <div className="flex flex-row gap-3 items-center">
+        <h1 className="text-lg md:text-xl lg:text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-indigo-400">
+          Surya Pratap Singh
+        </h1>
+        <p className="text-xs text-gray-400">Frontend & MERN Developer</p>
+      </div>
+
+      <div className="hidden md:flex gap-8">
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#about"
+        >
+          About
+        </a>
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#experience"
+        >
+          Experience
+        </a>
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#skills"
+        >
+          Tech
+        </a>
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#projects"
+        >
+          Projects
+        </a>
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#education"
+        >
+          Education
+        </a>
+        <a
+          className="text-gray-400 text-xs hover:text-purple-300 hover:font-bold"
+          href="#contact"
+        >
+          Contact
+        </a>
+      </div>
+    </div>
+  </header>
 )
 
 export default Navbar
