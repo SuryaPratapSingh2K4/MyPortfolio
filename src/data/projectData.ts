3575a7067dd2cc35bc073f9e80699cb22af2b1e6
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-11-09 14:09:41 +0530

    project completion

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -2,6 +2,8 @@ import bloodDonation from '../assets/Blood-Donation.mp4'
 import tripExpenser from '../assets/Trip-Expenser.mp4'
 import megaBlog from '../assets/Mega-Blog.mp4'
 import todo from '../assets/TODO.mp4'
+import VeridiaHiring from "../assets/Veridia-Hiring.mp4"
+import MERNNoteApp from "../assets/MERN-Note-App.mp4"
 
 export interface Project {
     title: string;
@@ -17,12 +19,13 @@ export const projects: Project[] = [
         title: "Veridia Hiring Platform",
         content: "Veridia is a full-stack hiring platform that streamlines recruitment by allowing candidates to apply for jobs and recruiters to manage applications with automated email updates and AWS-based resume uploads.",
         tech: ["React Js","Javascript","REST API","Tailwind CSS","Node Js","Express Js","Mongoose","MongoDB","Nodemailer","AWS S3","React-Router","JWT Auth"],
-        repo:"https://github.com/SuryaPratapSingh2K4/Veridia-Hiring-Platform"
+        repo:"https://github.com/SuryaPratapSingh2K4/Veridia-Hiring-Platform",
+        video:VeridiaHiring
     },
     {
         title: "Blood Donation Platform",
         content: "A web app that streamlines donor registration, blood requests, and record management. Enables users to add, update, and delete records with unique IDs for safe and organized data handling.",
-        tech: ["React Js","Javascript","REST API","Tailwind CSS","Node Js","Express Js","Mongoose","MongoDB","React-Router"],
+        tech: ["React Js","Javascript","REST API","Tailwind CSS","Node Js","Express Js","Mongoose","MongoDB","React-Router","JWT"],
         repo: "https://github.com/SuryaPratapSingh2K4/ReduxFunctionKeepNotes/tree/90678abca1ab89847777a16964c5527efd949e95/Blood-Donation",
         video:bloodDonation
     },
@@ -31,7 +34,7 @@ export const projects: Project[] = [
         content: "A secure MERN-based Notes App where users can log in, create, update, delete, and pin notes. Features JWT authentication, MongoDB storage, and real-time toast notifications for feedback.",
         tech: ["React Js","Tailwind CSS","Javascript","Node Js","Express Js","Mongoose","Mongo DB","REST API","React-Router","JWT Auth"],
         repo: "https://github.com/SuryaPratapSingh2K4/MERN-Note-App",
-        // video:"/assets/"
+        video:MERNNoteApp
     },
     {
         title: "Trip Expenser",
--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -7,10 +7,22 @@ const Education:React.FC = () => {
         <h3 className="text-3xl font-semibold text-purple-300 mb-4 flex items-center gap-2">
             <GraduationCap className="text-purple-400" /> Education
         </h3>
-        <div>
-            <h4 className="text-white font-medium text-lg">B.Tech in Information Technology</h4>
-            <p className="text-gray-400 text-sm">Odisha University of Technology and Research (2021–2025)</p>
-            <p className="text-gray-300 text-sm mt-1">CGPA: 7.4 / 10</p>
+        <div className='flex flex-col gap-4'>
+            <div className='flex flex-col'>
+                <h4 className="text-white font-medium text-lg">B.Tech in Information Technology</h4>
+                <p className="text-gray-400 text-sm italic">Odisha University of Technology and Research (2021–2025)</p>
+                <p className="text-gray-300 text-sm italic">CGPA: 7.4 / 10</p>
+            </div>
+            <div className='flex flex-col'>
+                <h4 className="text-white font-medium text-lg">Standard XII</h4>
+                <p className="text-gray-400 text-sm italic">LRDAV Public School (2021)</p>
+                <p className="text-gray-300 text-sm italic">Percentage: 88</p>
+            </div>
+            <div className='flex flex-col'>
+                <h4 className="text-white font-medium text-lg">Standard X</h4>
+                <p className="text-gray-400 text-sm italic">LRDAV Public School (2019)</p>
+                <p className="text-gray-300 text-sm italic">Percentage: 92</p>
+            </div>
         </div>
         </section>
     )
